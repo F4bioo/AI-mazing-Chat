3b390fc10dc34d75cca4e995e04cdb050bea1535
F4bioo <costa.fbo@gmail.com>
2023-01-22 15:18:47 -0300

    Initial commit

--- README.md
+++ README.md
@@ -5,4 +5,4 @@
 "AI-mazing Chat" is an artificial intelligence application for personalized conversations. It offers daily status updates and sharing. Perfect for studying languages or any topic in a fun and interactive way. This project uses GPT-3 technology from OpenAI, an API that provides personalized and enhanced conversations through artificial intelligence.
 
 
-<img src="1mockup.svg"/>
+<img src="images/1mockup.svg"/>
---
+++ app/src/main/java/com/fappslab/aimazingchat/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.fappslab.aimazingchat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fappslab.aimazingchat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
